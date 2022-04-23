4e36b527ef61af3778a225b291c1d6c3b499555b
iia.m.y <frezhgrand@gmail.com>
2022-04-24 01:46:17 +0300

    module builder implementation

---
+++ BsuirSchedule/Builder/ModuleBuilder.swift
@@ -0,0 +1,27 @@
+//
+//  ModuleBuilder.swift
+//  BsuirSchedule
+//
+//  Created by IlyaCool on 24.04.22.
+//
+
+import UIKit
+
+protocol Builder {
+    static func createScheduleModule() -> UIViewController
+}
+
+class ModuleBuilder: Builder {
+    static func createScheduleModule() -> UIViewController {
+        let networkService = NetworkService()
+        let scheduleView = ScheduleViewController()
+        let schedulePresenter = SchedulePresenter(view: scheduleView, schedule: nil)
+        networkService.getSchedule(forGroup: "051005") { schedule in
+            if let schedule = schedule {
+                schedulePresenter.schedule = schedule
+            }
+        }
+        scheduleView.presenter = schedulePresenter
+        return scheduleView
+    }
+}
